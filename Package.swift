4a570c460753992b4192c0aba408fdc339a1b495
Lennart Fischer <github@lambdadigamma.com>
2023-03-01 08:56:05 +0100

    Updated Package.swift to enable macOS v12 support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "ProseMirror",
-    platforms: [.iOS(.v14), .macOS(.v12), .watchOS(.v7), .tvOS(.v14)],
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12),
+        .watchOS(.v7),
+        .tvOS(.v14)
+    ],
     products: [
         .library(
             name: "ProseMirror",
